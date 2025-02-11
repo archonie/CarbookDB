72bf46a8614a264dceb165a3839a157f466b20f0
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2025-02-12 01:07:55 +0300

    Create Pricing and Location Controllers and CQRS classes

---
+++ Core /CarBook.Application/Features/Mediator/Commands/LocationCommands/RemoveLocationCommand.cs
@@ -0,0 +1,12 @@
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Commands.LocationCommands;
+
+public class RemoveLocationCommand: IRequest
+{
+    public int Id { get; set; }
+    public RemoveLocationCommand(int id)
+    {
+        Id = id;
+    }
+}
